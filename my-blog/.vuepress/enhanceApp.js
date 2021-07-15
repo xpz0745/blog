a662bdf27ccda8dbde21dc85bfcc0d6ed1ad1151
xuegao <xuegao@LyledeMBP.lan>
2021-07-15 20:26:51 +0800

    update

--- my-blog/.vuepress/enhanceApp.js
+++ my-blog/.vuepress/enhanceApp.js
@@ -1,9 +1,8 @@
 import WeChat from './components/wechat'
 import QQ from './components/qq'
-import BgRandom from './components/bgRandom'
 
 export default ({Vue}) => {
   Vue.component('WeChat', WeChat),
-  Vue.component('QQ', QQ),
-  Vue.component('BgRandom', BgRandom)
+  Vue.component('QQ', QQ)
 }
+
