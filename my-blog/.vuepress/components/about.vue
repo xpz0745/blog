7c24162c17d3c0a68ed05d073fcbd1d98c719001
xuegao <xuegao@LyledeMBP.lan>
2021-07-23 20:50:20 +0800

    完成博客关于页面

--- my-blog/.vuepress/components/about.vue
+++ my-blog/.vuepress/components/about.vue
@@ -1,55 +1,56 @@
 <template>
-	<div id="about">
-		<div class="about-nail">
-			<img src="http://121.40.49.66/img/nail.png" alt="图钉" />
-		</div>
-		<div class="about-side">
-			<div class="about-author">
-				<img src="http://121.40.49.66/img/authorAvatar.jpg" alt="作者头像" width="140px" height="140px" />
+	<div id="frame-about">
+		<div class="about">
+			<div class="about-nail">
+				<img src="http://121.40.49.66/img/nail.png" alt="图钉" />
 			</div>
-			<div class="about-authorInfo">
-				<ul>
-					<li><b>Name: </b>{{ Name }}</li>
-					<li><b>Profile: </b>{{ Profile }}</li>
-					<li><b>Email: </b>{{ Email }}</li>
-					<li><b>Hobby: </b>{{ Hobby }}</li>
-				</ul>
-			</div>
-			<div class="about-skill-Info">
-				<p>Skill</p>
-				HTML
-				<div class="container">
-				  <div class="skills html"></div>
-				</div><br>
-				CSS
-				<div class="container">
-				  <div class="skills css"></div>
-				</div><br>
-				Python
-				<div class="container">
-				  <div class="skills python"></div>
-				</div><br>
-				JavaScript
-				<div class="container">
-				  <div class="skills js"></div>
+			<div class="about-side">
+				<div class="about-author">
+					<el-avatar shape="square" :size="120" :fit="fit" :src="url"></el-avatar>
+				</div>
+				<div class="about-authorInfo">
+					<ul>
+						<li><b>Name: </b>{{ Name }}</li>
+						<li><b>Profile: </b>{{ Profile }}</li>
+						<li><b>Email: </b>{{ Email }}</li>
+						<li><b>Hobby: </b>{{ Hobby }}</li>
+					</ul>
+				</div>
+				<div class="about-skill-Info">
+					<p>Skill</p>
+					HTML
+					<div class="container">
+					  <div class="skills html"></div>
+					</div><br>
+					CSS
+					<div class="container">
+					  <div class="skills css"></div>
+					</div><br>
+					Python
+					<div class="container">
+					  <div class="skills python"></div>
+					</div><br>
+					JavaScript
+					<div class="container">
+					  <div class="skills js"></div>
+					</div>
 				</div>
 			</div>
+			<div class="about-main">
+				<h1>About me</h1><hr/>
+				<div class="about-content">
+					<p>
+						The original intention of setting up this blog website, 
+						is mainly to record the process of own growing up and learning.
+					</p>
+					<p class="about-time">
+						— {{ time }}
+					</p>
+				</div>
+			</div>	
 		</div>
-		<div class="about-main">
-			<h1>About me</h1><hr/>
-			<div class="about-content">
-				<p>
-					The original intention of setting up this blog website, 
-					is mainly to record the process of own growing up and learning.
-				</p>
-				<p class="about-time">
-					— {{ time }}
-				</p>
-			</div>
-		</div>	
 	</div>
-</template>	
-
+</template>
 <script>
 	export default {
 		data() {
@@ -59,22 +60,16 @@
 				Email: "313903714@qq.com",
 				Hobby: "League of Legends",
 				time: "",
-				clearId: ""
+				fit: 'cover',
+				url: 'http://121.40.49.66/img/authorAvatar.jpg'
 			}
 		},
-		created() {
+		mounted() {
 			this.handlerTime();
-		},
-		beforeRouteLeave(to,from,next) {
-			console.log(to);
-			clearInterval(this.clearId)
-			next()
 		},
 		methods: {
 			handlerTime() {
-				this.clearId = setInterval(()=>{
-					this.time = new Date().toLocaleString();
-				},1000)
+				this.time = new Date().toLocaleDateString();
 			}
 		}
 	}
--- my-blog/.vuepress/enhanceApp.js
+++ my-blog/.vuepress/enhanceApp.js
@@ -2,11 +2,16 @@ import WeChat from './components/wechat'
 import QQ from './components/qq'
 import BgRandom from './components/bgRandom'
 import About from './components/about'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+import 'element-ui/lib/theme-chalk/display.css'
+
 
 export default ({Vue}) => {
-  Vue.component('WeChat', WeChat),
+  Vue.component('WeChat', WeChat)
   Vue.component('QQ', QQ)
   Vue.component('BgRandom', BgRandom)
   Vue.component('About', About)
+  Vue.use(ElementUI)
 }
 
